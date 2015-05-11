9554dcd04d42b4de6f86f92174858cb25c124b98
Kelley Rose <krose422@gmail.com>
2015-05-11 13:23:18 -0400

    Changed JS file name Updated JS file with answers

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 var givenName;
 // Q: What does `givenName` equal right now?
+// A: undefined
 
 
 givenName = "Tim";
 // Q: What is `givenName` set to right now?
+// A: string of Tim
 
 
 givenName = "JD";
 // Q: What is `givenName` set to right now?
+// A: string of JD
 
 
 var greeting = "Hello, how are you?";
 // Q: What is `greeting` set to?
+// A: string of Hello, how are you?
 
 
 var high = 50;
@@ -20,10 +24,12 @@ var low  = 10;
 
 var math = high - low;
 // Q: What is `math` set to?
+// A: the number 40
 
 
 math = high - "5";
 // Q: What is `math` set to?
+// A: number 45
 
 
 // Create a variable to calculate Tim's age
@@ -31,10 +37,14 @@ math = high - "5";
 var born = 1983;
 var today = 2015;
 
+var age = "Tim is " + (today - born) + " years old.";
+
 
 
 // Store some information following in variables.
 var yourName;
 var instructorName;
+yourName = 'Kelley';
+instructorName = 'Tim';
 
 var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
